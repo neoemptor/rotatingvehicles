3c839fe62e64c9e01af0b3b4d897639a31571b91
Darren Bailey <darrensb@iinet.net.au>
2016-12-18 18:55:29 +0800

    Refactoring code for rotatingvehicles.js

--- js/rotatingvehicles.js
+++ js/rotatingvehicles.js
@@ -1,22 +1,51 @@
 // Author: Darren Bailey
-// 18-06-13
+// Created: 18-06-13
 // Email: darrensb@iinet.net.au
 // Project: Displays Rotating carousel menu of vehicles.
-// todo: border needs to be made so it overlays reflection instead of other way around.
+// Comments: There are four cars: ferrari, porche, jaguar and lamborghini.
+// ----------------------------------------------------------------------------------------------------------
 
-var stage;
-var queue;
-var step = 2 * Math.PI / 300; // the last number controls the fineness of
-// movement as opposed to jerky movement.
+// global constants
 
-// ferrari	porche	jaguar	lamborghini
-var h = 320;
-var k = 130;
-var r = 230;
-var theta = 0;
-var clockwise = 1;
-var anticlockwise = 0;
+// Eliptical orbit calculation.
+// Constants for calculating sine and cosine positions.
+
+// x = h + a cos t
+// y = k + b sin t
+
+// t = theta is the parameter, which ranges from 0 to 2π radians. That is a variable which is initialised further down the page.
+
+var elipseCentreX = 320; // h = Orginal horizontal x start position.
+var elipseCentreY = 130; // k = Orginal vertical y start position.
+var radius = 230; 		 // this is used in conjunction with scaleWidthBy(a) and scaleHeightBy(b)
+var scaleWidthBy = 1.15; // a is the scaling of the radius along the x axis.
+var scaleHeightBy = 0.4; // b is the scaling of the radius along the y axis.
+
+var sizeStep = .0025; 	 // Controls the fineness of scaling cars as they move to the back of the carousel and then the front. 
+
+// Car image rotation flags.
+var clockwise = true;
+var anticlockwise = false;
 var rotationDirection = clockwise;
+var offsetX = 31.1; // This is used for the positioning of the mouse hover image over the standard car image.
+var offsetY = 76.3; // This is used for the positioning of the mouse hover image over the standard car image.
+
+var sizeMin = .2; 	// size minimum limit 20%
+var sizeMax = 1; 	// size maximum limit 100%
+// ----------------------------------------------------------------------------------------------------------
+
+// global variables
+var stage;
+
+// This is for the eliptical rotation animation calculation. It steps up or down depending on the rotation clockwise or anti.
+// x = h + a cos t
+// y = k + b sin t
+// t = theta is the parameter, which ranges from 0 to 2π radians.
+var theta = 0;
+
+// The queue is a holding object which preloads images ready to be manipulated and displayed.
+var queue;
+var step = 2 * Math.PI / 300; // the last number controls the fineness of movement as opposed to jerky movement. This constant controls movement not size.
 
 // car reflections objects
 var aferrarireflect;
@@ -24,7 +53,7 @@ var aporchereflect;
 var ajaguarreflect;
 var alamborghinireflect;
 
-// car hover mouse glow objects
+// car mouse over glow objects
 var aferrariglow;
 var aporcheglow;
 var ajaguarglow;
@@ -38,30 +67,17 @@ var alamborghini;
 var bground;
 var abadge;
 
-var sizeStep = .0025; // controls the fineness of scaling cars as they move to the back of the carousel and then the front. 
-var sizeMin = .2; // size minimum limit 20%
-var sizeMax = 1; // size maximum limit 100%
-
-var ferrariYposBefore = 28;
-var ferrariYposAfter = 211;
-var porcheYposBefore = 28;
-var porcheYposAfter = 211;
-var jaguarYposBefore = 28;
-var jaguarYposAfter = 211;
-var lamborghiniYposBefore = 28;
-var lamborghiniYposAfter = 211;
-var moveFlag = 1; // used when mouse is over car image.
+var moveFlag = true; // Used when mouse is over car image and the user moves the mouse in a direction.
 
 var trackMouseDirection;
 
 function init() {
 	stage = new createjs.Stage("myCanvas");
 
+	// Enable touch tablet feature.
 	createjs.Touch.enable(stage);
 
-
 	queue = new createjs.LoadQueue(false);
-
 	queue.addEventListener("complete", handleComplete);
 	queue.loadManifest([
 		{
@@ -124,52 +140,45 @@ function init() {
 } // end init
 
 function tick(event) {
-	if (moveFlag == 1) {
-		// these are used as constants for the offset positions of the vehicle glows
-		offsetX = 31.1;
-		offsetY = 76.3;
+	if (moveFlag === true) {
+		updateImagePositions(aferrari, aferrarireflect, aferrariglow);
 
-		ferrariYposBefore = aferrari.y;
-		// car positions
-		aferrari.x = h + 1.15 * r * Math.cos(theta);
-		aferrari.y = k - 0.4 * r * Math.sin(theta);
-		aferrarireflect.x = aferrari.x;
-		aferrarireflect.y = aferrari.y + (aferrari.getBounds().height * aferrari.scaleY);
-		aferrariglow.x = aferrari.x - (offsetX * aferrari.scaleX);
-		aferrariglow.y = aferrari.y - (offsetY * aferrari.scaleY);
-		ferrariYposAfter = aferrari.y;
+		updateImagePositions(aporche, aporchereflect, aporcheglow);
 
-		porcheYposBefore = aporche.y;
-		//				 ^ this alters to make oval shape
-		aporche.x = h + 1.15 * r * Math.cos(theta + 1.5);
-		aporche.y = k - 0.4 * r * Math.sin(theta + 1.5);
-		aporchereflect.x = aporche.x;
-		aporchereflect.y = aporche.y + (aporche.getBounds().height * aporche.scaleY);
-		aporcheglow.x = aporche.x - (offsetX * aporche.scaleX);
-		aporcheglow.y = aporche.y - (offsetY * aporche.scaleY);
-		porcheYposAfter = aporche.y;
+		updateImagePositions(ajaguar, ajaguarreflect, ajaguarglow);
 
-		jaguarYposBefore = ajaguar.y;
-		ajaguar.x = h + 1.15 * r * Math.cos(theta + 3);
-		ajaguar.y = k - 0.4 * r * Math.sin(theta + 3);
-		ajaguarreflect.x = ajaguar.x;
-		ajaguarreflect.y = ajaguar.y + (ajaguar.getBounds().height * ajaguar.scaleY);
-		ajaguarglow.x = ajaguar.x - (offsetX * ajaguar.scaleX);
-		ajaguarglow.y = ajaguar.y - (offsetY * ajaguar.scaleY);
-		jaguarYposAfter = ajaguar.y;
+		updateImagePositions(alamborghini, alamborghinireflect, alamborghiniglow);
 
-		lamborghiniYposBefore = alamborghini.y;
-		alamborghini.x = h + 1.15 * r * Math.cos(theta + 4.5);
-		alamborghini.y = k - 0.4 * r * Math.sin(theta + 4.5);
-		alamborghinireflect.x = alamborghini.x;
-		alamborghinireflect.y = alamborghini.y + (alamborghini.getBounds().height * alamborghini.scaleY);
-		alamborghiniglow.x = alamborghini.x - (offsetX * alamborghini.scaleX);
-		alamborghiniglow.y = alamborghini.y - (offsetY * alamborghini.scaleY);
-		lamborghiniYposAfter = alamborghini.y;
-
-		sizeCars();
+		resizeCars();
 	}
-	// place cars and reflections on stage
+
+	// Place cars and reflections on stage.
+	placeCarsNReflectionsOnStage();
+
+	// Check if animation can continue.
+	checkAnimationContinue();
+	stage.update();
+} // end tick
+
+function checkAnimationContinue() {
+	if (moveFlag === true) {
+		if (rotationDirection === clockwise) {
+			theta += step;
+			// Check if you need to reset the plotting of the eliptical path of image objects motion.
+			if (theta >= 2 * Math.PI) {
+				theta = 0;
+			}
+		}
+		else { // rotate anticlockwise
+			theta -= step;
+			if (theta <= 0) {
+				theta = 2 * Math.PI - step;
+			}
+		}
+	}
+} // end checkAnimationContinue
+
+function placeCarsNReflectionsOnStage() {
 	stage.addChild(aferrarireflect);
 	stage.addChild(aporchereflect);
 	stage.addChild(ajaguarreflect);
@@ -179,49 +188,41 @@ function tick(event) {
 	stage.addChild(aporche);
 	stage.addChild(ajaguar);
 	stage.addChild(alamborghini);
+} // end placeCarsNReflectionsOnStage
 
-	// check if animation can continue
-	if (moveFlag == 1) {
-		if (rotationDirection == clockwise) {
-			theta += step;
-			// check if you need to reset plotting of oval 
-			if (theta >= 2 * Math.PI) {
-				theta = 0;
-			}
-		}
-		else {
-			theta -= step;
-			if (theta <= 0) {
-				theta = 2 * Math.PI - step;
-			}
-		}
-	}
-	stage.update();
-
-	//" porche scaleY:"+aporche.scaleY+" jaguar scaleY:"+ajaguar.scaleY+" lamborghini scaleY:"+alamborghini.scaleY);
-} // end tick
+function updateImagePositions(theCar, theCarReflect, theCarGlow) {
+	theCar.YposBefore = theCar.y;
+	// car positions
+	theCar.x = elipseCentreX + scaleWidthBy * radius * Math.cos(theta + theCar.offset);
+	theCar.y = elipseCentreY - scaleHeightBy * radius * Math.sin(theta + theCar.offset);
+	theCarReflect.x = theCar.x;
+	theCarReflect.y = theCar.y + (theCar.getBounds().height * theCar.scaleY);
+	theCarGlow.x = theCar.x - (offsetX * theCar.scaleX);
+	theCarGlow.y = theCar.y - (offsetY * theCar.scaleY);
+	theCar.YposAfter = theCar.y;
+} // end updateImagePositions
 
 function handleMouse(event) {
 	switch (event.type) {
 		case "mouseover":
-			moveFlag = 0; // to pause animation
+			moveFlag = false; // to pause animation
 			trackMouseDirection = event.stageX;
 			switch (event.target.name) {
-				case "ferrari": // hilight ferrari
+				case "ferrari": // highlight ferrari
 					stage.addChild(aferrariglow);
 					break;
-				case "porche": // hilight porche
+				case "porche": // highlight porche
 					stage.addChild(aporcheglow);
 					break;
-				case "jaguar": // hilight jaguar
+				case "jaguar": // highlight jaguar
 					stage.addChild(ajaguarglow);
 					break;
-				case "lamborghini": // hilight lamborghini
+				case "lamborghini": // highlight lamborghini
 					stage.addChild(alamborghiniglow);
 			}
 			break;
 		case "mouseout":
-			moveFlag = 1; // resume animation
+			moveFlag = true; // resume animation
 			switch (event.target.name) {
 				case "ferrari":
 					stage.removeChild(aferrariglow);
@@ -236,14 +237,14 @@ function handleMouse(event) {
 					stage.removeChild(alamborghiniglow);
 					break;
 			}
-			if (trackMouseDirection < event.stageX) {
+			if (trackMouseDirection < event.stageX) { // If the user is moving the mouse to the left change rotation direction anticlockwise.
 				rotationDirection = anticlockwise;
 			}
-			else if (trackMouseDirection > event.stageX) {
+			else if (trackMouseDirection > event.stageX) { // If the user is moving the mouse to the right change rotation direction clockwise.
 				rotationDirection = clockwise;
 			}
 			break;
-		case "click":
+		case "click":	// Go to the page related to the image.
 			switch (event.target.name) {
 				case "ferrari":
 					window.location.assign("ferrari.html");
@@ -262,6 +263,61 @@ function handleMouse(event) {
 
 function handleComplete(event) {
 	// create images
+	createImages();
+
+	// initialise mouseover cursors
+	initMouseoverCursors();
+
+	// set initial defaults for image objects
+	// ---------------------------------------------------------------------------------------------------------------------------------------
+	// used to reference objects in mouse handler
+	aferrari.name = "ferrari";
+	aporche.name = "porche";
+	ajaguar.name = "jaguar";
+	alamborghini.name = "lamborghini";
+
+	// initialise car scales
+	initCarScales();
+
+	// These are the offset starting positions for each car image. The Ferrari is the original calculated position by which all other cars are offset from.
+	// Therefore the offset for the Ferrari is 0.
+	initOffsetPositions();
+
+	// Initialise vertical before and after positions for saving state when position transformed.
+	initBeforeAfterPositions();
+
+	// Add event listeners.
+	addEventListeners();
+
+	// Initialise and place the background. These are considered constants.
+	var staticItemScale = .6; // 60%
+
+	bground.scaleX = staticItemScale;
+	bground.scaleY = staticItemScale;
+	stage.addChild(bground);
+
+	// Initialise the badge which is placed on top of the background but behind the cars. These are considered constants.
+	abadge.x = 220;
+	abadge.y = 120;
+	abadge.scaleX = staticItemScale;
+	abadge.scaleY = staticItemScale;
+	stage.addChild(abadge);
+
+	// Add the tick function to the timer.
+	createjs.Ticker.addEventListener("tick", tick);
+} // end handleComplete
+
+function initOffsetPositions() {
+	// Treat these as constants.
+	// These are the positions the cars will be placed around the eliptical path which they will then move along the eliptical path.
+	aferrari.offset = 0;
+	aporche.offset = 1.5;
+	ajaguar.offset = 3;
+	alamborghini.offset = 4.5;
+
+} // end initOffsetPositions
+
+function createImages() {
 	bground = new createjs.Bitmap(queue.getResult("background"));
 	abadge = new createjs.Bitmap(queue.getResult("badge"));
 
@@ -279,42 +335,72 @@ function handleComplete(event) {
 	aporcheglow = new createjs.Bitmap(queue.getResult("porcheglow"));
 	ajaguarglow = new createjs.Bitmap(queue.getResult("jaguarglow"));
 	alamborghiniglow = new createjs.Bitmap(queue.getResult("lamborghiniglow"));
+} // createImages
 
-	// used to reference objects in mouse handler
-	aferrari.name = "ferrari";
-	aporche.name = "porche";
-	ajaguar.name = "jaguar";
-	alamborghini.name = "lamborghini";
+function initBeforeAfterPositions() {
+	var startX = 28;
+	var startY = 211;
 
-	// set initial defaults for objects
-	aferrari.scaleX = .75;
-	aferrari.scaleY = .75;
-	aporche.scaleX = .5
-	aporche.scaleY = .5
-	ajaguar.scaleX = .75;
-	ajaguar.scaleY = .75;
-	alamborghini.scaleX = 1
-	alamborghini.scaleY = 1
+	aferrari.YposBefore = startX;
+	aferrari.YposAfter = startY;
+	aporche.YposBefore = startX;
+	aporche.YposAfter = startY;
+	ajaguar.YposBefore = startX;
+	ajaguar.YposAfter = startY;
+	alamborghini.YposBefore = startX;
+	alamborghini.YposAfter = startY;
+} // initBeforeAfterPositions
 
-	aferrarireflect.scaleX = .75;
-	aferrarireflect.scaleY = .75;
-	aporchereflect.scaleX = .5
-	aporchereflect.scaleY = .5
-	ajaguarreflect.scaleX = .75; 
-	ajaguarreflect.scaleY = .75; 
-	alamborghinireflect.scaleX = 1
-	alamborghinireflect.scaleY = 1
+function initMouseoverCursors() {
+	var theCursor = "pointer";
 
-	aferrariglow.scaleX = .75;
-	aferrariglow.scaleY = .75;
-	aporcheglow.scaleX = .5;
-	aporcheglow.scaleY = .5;
-	ajaguarglow.scaleX = .75;
-	ajaguarglow.scaleY = .75;
-	alamborghiniglow.scaleX = 1;
-	alamborghiniglow.scaleY = 1;
+	aferrariglow.cursor = theCursor;
+	aporcheglow.cursor = theCursor;
+	ajaguarglow.cursor = theCursor;
+	alamborghiniglow.cursor = theCursor;
+
+	aferrari.cursor = theCursor;
+	aporche.cursor = theCursor;
+	ajaguar.cursor = theCursor;
+	alamborghini.cursor = theCursor;
+} // initMouseoverCursors
+
+function initCarScales() {
+	var scaleLamborghini = 1;
+	var scaleFerrari = .75;
+	var scaleJaguar = scaleFerrari;
+	var scalePorche = .5;
 
 
+	aferrari.scaleX = scaleFerrari;
+	aferrari.scaleY = scaleFerrari;
+	aporche.scaleX = scalePorche;
+	aporche.scaleY = scalePorche;
+	ajaguar.scaleX = scaleJaguar;
+	ajaguar.scaleY = scaleJaguar;
+	alamborghini.scaleX = scaleLamborghini;
+	alamborghini.scaleY = scaleLamborghini;
+
+	aferrarireflect.scaleX = scaleFerrari;
+	aferrarireflect.scaleY = scaleFerrari;
+	aporchereflect.scaleX = scalePorche;
+	aporchereflect.scaleY = scalePorche;
+	ajaguarreflect.scaleX = scaleJaguar;
+	ajaguarreflect.scaleY = scaleJaguar;
+	alamborghinireflect.scaleX = scaleLamborghini;
+	alamborghinireflect.scaleY = scaleLamborghini;
+
+	aferrariglow.scaleX = scaleFerrari;
+	aferrariglow.scaleY = scaleFerrari;
+	aporcheglow.scaleX = scalePorche;
+	aporcheglow.scaleY = scalePorche;
+	ajaguarglow.scaleX = scaleJaguar;
+	ajaguarglow.scaleY = scaleJaguar;
+	alamborghiniglow.scaleX = scaleLamborghini;
+	alamborghiniglow.scaleY = scaleLamborghini;
+} // end initCarScales
+
+function addEventListeners() {
 	aferrari.addEventListener("click", handleMouse);
 	aporche.addEventListener("click", handleMouse);
 	ajaguar.addEventListener("click", handleMouse);
@@ -329,60 +415,27 @@ function handleComplete(event) {
 	aporche.addEventListener("mouseout", handleMouse);
 	ajaguar.addEventListener("mouseout", handleMouse);
 	alamborghini.addEventListener("mouseout", handleMouse);
+} // end addEventListeners
 
-	// initialise the background
-	bground.scaleX = .6;
-	bground.scaleY = .6;
-	stage.addChild(bground);
-	// initialise the badge which is placed on top of the background but behind the cars
-	abadge.x = 220;
-	abadge.y = 120;
-	abadge.scaleX = .6;
-	abadge.scaleY = .6;
-	stage.addChild(abadge);
-
-	// add the tick function to the timer
-	createjs.Ticker.addEventListener("tick", tick);
-} // end handleComplete
-
-function sizeCars() {
+function resizeCars() {
 	// car sizing
-	aferrari.scaleX = getSize(ferrariYposBefore, ferrariYposAfter, aferrari.scaleY);
-	aferrari.scaleY = aferrari.scaleX;
-	aferrarireflect.scaleX = aferrari.scaleX;
-	aferrarireflect.scaleY = aferrari.scaleX;
-	aferrariglow.scaleX = aferrari.scaleX;
-	aferrariglow.scaleY = aferrari.scaleX;
+	setCarSize(aferrari, aferrarireflect, aferrariglow);
+	setCarSize(aporche, aporchereflect, aporcheglow);
+	setCarSize(ajaguar, ajaguarreflect, ajaguarglow);
+	setCarSize(alamborghini, alamborghinireflect, alamborghiniglow);
+} // end resizeCars
 
-	aporche.scaleX = getSize(porcheYposBefore, porcheYposAfter, aporche.scaleY);
-	aporche.scaleY = aporche.scaleX;
-	aporchereflect.scaleX = aporche.scaleX;
-	aporchereflect.scaleY = aporche.scaleX;
-	aporcheglow.scaleX = aporche.scaleX;
-	aporcheglow.scaleY = aporche.scaleX;
-
-	ajaguar.scaleX = getSize(jaguarYposBefore, jaguarYposAfter, ajaguar.scaleY);
-	ajaguar.scaleY = ajaguar.scaleX;
-	ajaguarreflect.scaleX = ajaguar.scaleX;
-	ajaguarreflect.scaleY = ajaguar.scaleX;
-	ajaguarglow.scaleX = ajaguar.scaleX;
-	ajaguarglow.scaleY = ajaguar.scaleX;
-
-	alamborghini.scaleX = getSize(lamborghiniYposBefore, lamborghiniYposAfter, alamborghini.scaleY);
-	alamborghini.scaleY = alamborghini.scaleX;
-	alamborghinireflect.scaleX = alamborghini.scaleX;
-	alamborghinireflect.scaleY = alamborghini.scaleX;
-	alamborghiniglow.scaleX = alamborghini.scaleX;
-	alamborghiniglow.scaleY = alamborghini.scaleX;
-} // end sizeCarsUp 
-
-function getSize(posBefore, posAfter, thisSize) {
-
-	if (posBefore < posAfter && thisSize <= sizeMax) {
-		thisSize += sizeStep;
+function setCarSize(aCar, aCarReflect, aCarGlow) {
+	if (aCar.YposBefore < aCar.YposAfter && aCar.scaleY <= sizeMax) {
+		aCar.scaleY += sizeStep;
+		aCar.scaleX += sizeStep;
 	}
-	else if (posBefore > posAfter && thisSize >= sizeMin) {
-		thisSize -= sizeStep;
+	else if (aCar.YposBefore > aCar.YposAfter && aCar.scaleY >= sizeMin) {
+		aCar.scaleY -= sizeStep;
+		aCar.scaleX -= sizeStep;
 	}
-	return thisSize;
-}
+	aCarReflect.scaleX = aCar.scaleX;
+	aCarReflect.scaleY = aCar.scaleX;
+	aCarGlow.scaleX = aCar.scaleX;
+	aCarGlow.scaleY = aCar.scaleX;
+} // end setCarSize
